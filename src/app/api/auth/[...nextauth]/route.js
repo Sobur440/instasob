46c0fb69652f48402930c3f7fac3f33a09320665
sobur440 <abubakarsobur440@gmail.com>
2024-05-02 12:21:31 +0100

    added next auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+export const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
+export { handler as GET, handler as POST };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 function Header() {
+  const { data: session } = useSession();
+
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center  max-w-6xl mx-auto">
@@ -15,7 +21,21 @@ function Header() {
           className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
         />
         {/* Menu Items */}
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+        {session ? (
+          <img
+            src={session.user.image}
+            alt={session.user.name}
+            className="w-10 h-10 rounded-[50%] cursor-pointer"
+            onClick={() => signOut()}
+          />
+        ) : (
+          <button
+            className="text-sm font-semibold text-blue-500"
+            onClick={() => signIn()}
+          >
+            Log In
+          </button>
+        )}
       </div>
     </div>
   );
